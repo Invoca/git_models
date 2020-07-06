5bc984453e75aa58c301a4921489e575c7c4fd0d
Colin Kelley <colindkelley@gmail.com>
2020-07-06 15:37:01 -0700

    TECH-4588: add .rspec and update spec_helper

--- spec/dummy/spec/spec_helper.rb
+++ spec/dummy/spec/spec_helper.rb
@@ -10,6 +10,8 @@ require 'git_models/test_helpers'
 require 'database_cleaner'
 require 'rake'
 require 'rspec/rails'
+require 'rspec_junit_formatter'
+require 'climate_control'
 
 RSpec.configure do |config|
   config.before(:suite) do
@@ -21,5 +23,24 @@ RSpec.configure do |config|
     DatabaseCleaner.cleaning do
       example.run
     end
+  end
+
+  config.add_formatter RspecJunitFormatter, ENV['JUNIT_OUTPUT'] || 'spec/reports/rspec.xml'
+
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = "spec/reports/.rspec_status"
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  def with_modified_env(options, &block)
+    ClimateControl.modify(options, &block)
   end
 end
